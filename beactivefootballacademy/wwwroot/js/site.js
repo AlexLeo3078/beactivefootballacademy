53e36266935837151610fde7081f1e72dd2f5538
Alessandro Leo <alessandro.leo@iar.com>
2023-11-09 14:44:08 +0000

    reactive navbar

--- beactivefootballacademy/wwwroot/js/site.js
+++ beactivefootballacademy/wwwroot/js/site.js
@@ -14,3 +14,17 @@ if (document.getElementById('galleria') != null) {
         wait: true
     });
 }
+
+
+var lastScrollTop = 0;
+$('.navbar').css('top', '0');
+
+window.addEventListener("scroll", function () {
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop) {
+        $('.navbar').css('top', '-10rem');
+    } else if (st < lastScrollTop) {
+        $('.navbar').css('top', '0');
+    } // else was horizontal scroll
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
